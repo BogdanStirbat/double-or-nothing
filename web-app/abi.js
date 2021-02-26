87d6e8f4f82017d5b52a04889b0436de744789bc
Bogdan Stirbat <bogdan.stirbat@gmail.com>
2021-02-27 00:33:10 +0200

    added a basic frontend app

--- web-app/abi.js
+++ web-app/abi.js
@@ -1,3 +1,93 @@
 var abi = [
-  
+  {
+    "inputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "name": "player",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "BetWon",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "name": "player",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "name": "amount",
+        "type": "uint256"
+      }
+    ],
+    "name": "BetLost",
+    "type": "event"
+  },
+  {
+    "constant": false,
+    "inputs": [],
+    "name": "addFunds",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [],
+    "name": "withdrawFunds",
+    "outputs": [],
+    "payable": false,
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "constant": true,
+    "inputs": [],
+    "name": "getBettingLimit",
+    "outputs": [
+      {
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "payable": false,
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "constant": false,
+    "inputs": [],
+    "name": "bet",
+    "outputs": [
+      {
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "payable": true,
+    "stateMutability": "payable",
+    "type": "function"
+  }
 ]
--- web-app/main.js
+++ web-app/main.js
@@ -2,8 +2,59 @@ var web3 = new Web3(Web3.givenProvider);
 var contractInstance;
 
 $(document).ready(function() {
-    window.ethereum.enable().then(function(accounts) {
-        contractInstance = new web3.eth.Contract(abi, "0x03F37Ec985396B4a957c3A06b72576Bd131189C5", {from: accounts[0]});
-        console.log(contractInstance);
-    });
+  window.ethereum.enable().then(function(accounts) {
+    contractInstance = new web3.eth.Contract(abi, "0x4930f5e13dA35463fFbf00C7546FB04813a86F5d", {from: accounts[0]});
+    console.log(contractInstance);
+
+    retrieveBettingLimit()
+
+    $("#add_funds").click(addFunds)
+    $("#start_betting").click(bet)
+  });
 });
+
+function retrieveBettingLimit() {
+  contractInstance.methods.getBettingLimit().call().then(function(res) {
+    $("#max_amount").text(`${res} wei`);
+  })
+}
+
+function addFunds() {
+  let funds = $("#add_funds_value").val()
+
+  contractInstance.methods.addFunds().send({value: funds})
+  .on("transactionHash", function(hash) {
+      console.log("transactionHash")
+  })
+  .on("confirmation", function(confirmationNr) {
+      console.log("confirmation")
+  })
+  .on("receipt", function(receipt) {
+      console.log("receipt")
+      retrieveBettingLimit()
+  })
+}
+
+function bet() {
+  let betValue = $("#bet_value").val()
+  console.log(betValue)
+
+  contractInstance.methods.bet().send({value: betValue})
+  .on("transactionHash", function(hash) {
+    console.log("transactionHash")
+  })
+  .on("confirmation", function(confirmationNr) {
+    console.log("confirmation")
+  })
+  .on("receipt", function(receipt) {
+    console.log("receipt")
+    console.log(receipt)
+    if (receipt.events.BetLost) {
+        $("#betting_result").text("lost")
+    }
+    if (receipt.events.BetWon) {
+        $("#betting_result").text("won")
+    }
+    retrieveBettingLimit()
+  })
+}
